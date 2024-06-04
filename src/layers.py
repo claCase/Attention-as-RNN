7a09c4aced55a2527db7c3ce0cd8e09c7003abc9
Claudio_Casellato <claudiocasellatoml@gmail.com>
2024-06-04 20:46:06 +0200

    get_config of scan layer

--- src/layers.py
+++ src/layers.py
@@ -1,17 +1,16 @@
 from tensorflow_probability.python.math import scan_associative
 import tensorflow as tf
 from tensorflow.python.keras import layers, models, activations, losses, backend as tfk
-from tensorflow.python.keras.layers.recurrent import (
-    DropoutRNNCellMixin,
-    _config_for_enable_caching_device,
-    _caching_device,
-)
+from packaging.version import Version
+
+if Version(tf.__version__) >= Version("2.16.0"):
+    from keras.src.layers.rnn.dropout_rnn_cell import DropoutRNNCell as DropCell
+else:
+    from tensorflow.python.keras.layers.recurrent import DropoutRNNCellMixin as DropCell
 import numpy as np
 
 
-class AttentionRNNCell(
-    DropoutRNNCellMixin, tf.keras.__internal__.layers.BaseRandomLayer
-):
+class AttentionRNNCell(DropCell, layers.Layer):
     def __init__(
         self,
         heads,
@@ -37,26 +36,18 @@ class AttentionRNNCell(
         ]
         self.output_size = tf.TensorShape([heads, dim])
 
-        if tf.compat.v1.executing_eagerly_outside_functions():
-            self._enable_caching_device = kwargs.pop("enable_caching_device", True)
-        else:
-            self._enable_caching_device = kwargs.pop("enable_caching_device", False)
-
     def build(self, input_shape):
-        default_caching_device = _caching_device(self)
         i = input_shape[-1]
         self.kv_kernel = self.add_weight(
             "kv_kernel",
             shape=(i, self.heads, self.dim, 2),
-            caching_device=default_caching_device,
         )
         self.q_kernel = self.add_weight(
             "q_kernel",
             shape=(self.heads, self.dim),
-            caching_device=default_caching_device,
         )
 
-    # @tf.function
+    @tf.function
     def call(self, inputs, states, training=False):
         h, prev_num, prev_den, prev_max = states
         prev_max = tf.squeeze(prev_max, -1)
@@ -114,12 +105,11 @@ class AttentionRNNCell(
         # Denominator recurrence
         ck = prev_den * exp_max_diff + sm  # BH
         # Numerator recurrence
-        ak = (
-            prev_num * exp_max_diff[..., None] + value * sm[..., None]
-        )  # BHO
+        ak = prev_num * exp_max_diff[..., None] + value * sm[..., None]  # BHO
         return ak, ck, curr_max
 
 
+@tf.keras.utils.register_keras_serializable("RNNAttention")
 class ScanAssociativeRNNAttention(layers.Layer):
     def __init__(
         self,
@@ -136,7 +126,7 @@ class ScanAssociativeRNNAttention(layers.Layer):
         self.dim = dim
         self.dropout = layers.Dropout(max(min(dropout, 1), 0))
         self.activation = activations.get(activation)
-        
+
     def build(self, input_shape):
         i = input_shape[-1]
         self.kv_kernel = self.add_weight(
@@ -195,8 +185,8 @@ class ScanAssociativeRNNAttention(layers.Layer):
         """Associative operator ⊕ acting on 3-tuples
 
         Args:
-            a (Tuple[m, w, u]): even indices of split sequence 
-            b (Tuple[m, w, u]): odd indices of split sequence  
+            a (Tuple[m, w, u]): even indices of split sequence
+            b (Tuple[m, w, u]): odd indices of split sequence
 
         Returns:
             c (Tuple[m, w, u]):
@@ -219,6 +209,9 @@ class ScanAssociativeRNNAttention(layers.Layer):
             inputs (tf.Tensor): (B, T, D)
             training (bool): True if training else False
         """
+        return self.scan(inputs, training)[0]
+
+    def scan(self, inputs, training):
         shape = tf.shape(inputs)
         B = shape[0]
         T = shape[1]
@@ -243,4 +236,67 @@ class ScanAssociativeRNNAttention(layers.Layer):
             o = tf.reshape(h, (B, -1))
         else:
             o = tf.reduce_sum(h, -2)
-        return o
+        return o, m, c, a
+
+    def recurrence(self, query, key, value, prev_num, prev_den, prev_max, cache=None):
+        """Computes softmax recurrence
+
+        Args:
+            query (tf.Tensor): Tensor of shape (B,H,O)
+            key (tf.Tensor): Tensor of shape (B,H,O)
+            value (tf.Tensor): Tensor of shape (B,H,O)
+            prev_num (tf.Tensor): Tensor of shape (B,H,O)
+            prev_den (tf.Tensor): Tensor of shape (B,H)
+            prev_max (tf.Tensor): Tensor of shape (B,H)
+            cache (tf.Tensor, optional): Cache. Defaults to None.
+
+        Returns:
+            Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:  (numerator, denominator, max)
+        """
+        # Query-Key inner product
+        s = tf.einsum("...q,...q->...", query, key)  # BH
+        # Update max for stable soft-max
+        curr_max = tf.maximum(prev_max, s)  # BH
+        exp_max_diff = tf.math.exp(prev_max - curr_max)  # BH
+        # Subtract max to stabilize
+        sm = tf.math.exp(s - curr_max)  # BH
+        # Denominator recurrence
+        ck = prev_den * exp_max_diff + sm  # BH
+        # Numerator recurrence
+        ak = prev_num * exp_max_diff[..., None] + value * sm[..., None]  # BHO
+        return ak, ck, curr_max
+
+    def autoregressive_sample(self, inputs, length, stochastic=False):
+        samples = tf.TensorArray(dtype=tf.float32, size=length)
+        o, prev_max, prev_den, prev_num = self.scan(inputs, training=stochastic)
+        prev_max, prev_den, prev_num = (
+            prev_max[:, -1],
+            prev_den[:, -1],
+            prev_num[:, -1],
+        )
+        q = self.q_kernel
+        kv = tf.einsum("...i,ihok->...hok", inputs[:, -1], self.kv_kernel)
+        kv = self.activation(kv)
+        kv = self.dropout(kv, training=stochastic)
+        k, v = tf.split(kv, 2, -1)
+        
+        for t in tf.range(length):
+            prev_num, prev_den, prev_max = self.recurrence(q, k, v, prev_num, prev_den, prev_max)
+
+    def get_config(self):
+        config = {
+            "dropout": self.dropout,
+            "heads": self.attn_heads,
+            "dim": self.dim,
+            "concat_heads": self.concat_heads,
+            "activation": self.activation
+                if type(self.activation) is str
+                else tf.keras.utils.serialize_keras_object(self.activation),
+            "initializer": (
+                self.initializer
+                if type(self.initializer) is str
+                else tf.keras.utils.serialize_keras_object(self.initializer)
+            ),
+        }
+        base_config = super().get_config()
+        return dict(list(base_config.items()) + list(config.items()))
