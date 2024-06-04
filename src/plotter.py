e332812d90f231b03fb56e2fed796552074079d8
Claudio_Casellato <claudiocasellatoml@gmail.com>
2024-06-04 21:53:27 +0200

    color update and savepath

--- src/plotter.py
+++ src/plotter.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 import numpy as np
 
-def plot_hist2d(model, inputs, samples=200, axis=0, resolution=500):
+def plot_hist2d(model, inputs, samples=200, axis=0, resolution=500,save_path=None):
     assert len(inputs.shape) == 3 and inputs.shape[0] == 1
 
     samples = max(samples, 4)
@@ -14,8 +14,7 @@ def plot_hist2d(model, inputs, samples=200, axis=0, resolution=500):
     Y = np.asarray(Y)
     length = Y.shape[1]
     X = np.linspace(0, 1, length)
-    #X = np.broadcast_to(X, (samples, length))
-
+    
     x_fine = np.linspace(X.min(), X.max(), resolution)
     y_fine = np.concatenate([np.interp(x_fine, X, y_row) for y_row in Y])
     x_fine = np.broadcast_to(x_fine, (samples, resolution)).ravel()
@@ -26,9 +25,11 @@ def plot_hist2d(model, inputs, samples=200, axis=0, resolution=500):
     ax.pcolormesh(ex, ey, h.T, cmap="inferno")  
     for i in range(4):
         if i == 0:
-            ax.plot(X, Y[i], color="blue", label="RNN-Attention", alpha=0.5, linewidth=1)
+            ax.plot(X, Y[i], color="white", label="RNN-Attention", alpha=0.5, linewidth=1)
         else:
-            ax.plot(X, Y[i], color="blue", alpha=0.5, linewidth=1)
-    ax.plot(X, inputs[0, :, axis], label="True")
+            ax.plot(X, Y[i], color="white", alpha=0.5, linewidth=1)
+    ax.plot(X, inputs[0, :, axis], color="lime",label="True", linewidth=2)
     ax.legend()
+    if save_path is not None:
+        fig.savefig(save_path)
     return fig, ax 
